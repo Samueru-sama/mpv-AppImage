daa7bc624906a58e1b8f98da58986fc7e7a57ff3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-09-04 01:14:38 -0400

    Update mpv-AppImage.sh

--- mpv-AppImage.sh
+++ mpv-AppImage.sh
@@ -1,55 +1,40 @@
 #!/bin/sh
 
-# THIS SCRIPT BUILDS MPV DEPENDENCIES AND MPV STATICALLY USING THIS: https://github.com/mpv-player/mpv-build
+# dependencies: 
+# meson cmake automake ninja ninja-build vulkan-headers freetype-dev libass-dev
+# fribidi-dev harfbuzz-dev yasm libx11 libx11-dev libxinerama-dev libxrandr-dev 
+# libxscrnsaver libxscrnsaver-dev xscreensaver-gl-extras jack libpulse pulseaudio-dev
+# rubberband libcaca mesa-egl libxpresent-dev lua5.3-dev libxcb-dev desktop-file-utils
+
 set -u
 export ARCH=x86_64
 export APPIMAGE_EXTRACT_AND_RUN=1
-APP=mpv
-APPDIR="$APP".AppDir
 REPO="https://github.com/mpv-player/mpv-build.git"
-EXEC="$APP"
-APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/[()",{} ]/\n/g' | grep -oi 'https.*continuous.*tool.*x86_64.*mage$' | head -1)
+GOAPPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - \
+	| sed 's/[()",{} ]/\n/g' | grep -oi 'https.*continuous.*tool.*x86_64.*mage$' | head -1)
+APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
+mkdir -p ./mpv/mpv.AppDir && cd ./mpv/mpv.AppDir || exit 1
 
-# make vulkan headers
-wget https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.3.238.tar.gz
-tar fx *tar* && cd Vulkan*
-cmake -S . -B build/
-sudo cmake --install build --prefix '/usr'
-cd .. && rm -rf ./*tar* ./Vulkan*
-
-# CREATE DIRECTORIES AND BUILD MPV
-[ -n "$APP" ] && mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
-CURRENTDIR="$(readlink -f "$(dirname "$0")")" # DO NOT MOVE THIS
-git clone "$REPO" && cd ./mpv-build && sed -i "s#meson setup build#meson setup build -Dprefix=$CURRENTDIR/usr#g" ./scripts/mpv-config \
-&& ./rebuild -j$(nproc) && ./install && cd .. && rm -rf ./mpv-build || exit 1
-
-# AppRun
-cat >> ./AppRun << 'EOF'
-#!/bin/sh
-CURRENTDIR="$(dirname "$(readlink -f "$0")")"
-if [ -z "$@" ]; then
-	"$CURRENTDIR/usr/bin/mpv" --player-operation-mode=pseudo-gui
-else
-	"$CURRENTDIR/usr/bin/mpv" "$@"
+# Build mpv
+if [ ! -d ./usr ]; then
+	CURRENTDIR="$(readlink -f "$(dirname "$0")")"
+	git clone "$REPO" && cd ./mpv-build || exit 1
+	sed -i "s#meson setup build#meson setup build -Dprefix=$CURRENTDIR/usr#g" ./scripts/mpv-config
+	./rebuild -j$(nproc) && ./install && cd .. && rm -rf ./mpv-build || exit 1
+	cp ./usr/share/icons/hicolor/128x128/apps/mpv.png ./ && ln -s ./mpv.png ./.DirIcon
 fi
-EOF
-chmod a+x ./AppRun
-export VERSION=$(./AppRun --version | awk 'FNR == 1 {print $2}')
-cp ./usr/share/icons/hicolor/128x128/apps/mpv.png ./ && ln -s ./mpv.png ./.DirIcon # If not done linuxdeploy will pick the wrong icon
 
-# MAKE APPIMAGE USING FUSE3 COMPATIBLE APPIMAGETOOL
-cd .. && cp -r "$APPDIR" "$APPDIR"2 && wget -q "$APPIMAGETOOL" -O ./appimagetool && chmod a+x ./appimagetool || exit 1
-
-./appimagetool deploy "$APPDIR"/usr/share/applications/*.desktop || exit 1
-sed -i 's/export PYTHONHOME/#export PYTHONHOME/g' "$APPDIR"/AppRun # unsets this since python isn't bundled
-./appimagetool -s ./"$APPDIR" || exit 1
-mv ./*.AppImage .. && echo "Regular appimage made" || exit 1
-
-# EXPERIMENTAL DEPLOY EVERYTHING MODE. TODO FIX INTERNET ISSUES
-APPDIR="$APPDIR"2
-export VERSION="$VERSION-anylinux"
-sed -i 's/Name=mpv/Name=WIP-mpv/g' "$APPDIR"/usr/share/applications/*.desktop
-./appimagetool -s deploy "$APPDIR"/usr/share/applications/*.desktop || exit 1
-sed -i 's/export PYTHONHOME/#export PYTHONHOME/g' "$APPDIR"/AppRun # unsets this since python isn't bundled
-./appimagetool -s ./"$APPDIR" || exit 1
-[ -n "$APP" ] && mv ./*.AppImage .. && cd .. && rm -rf ./"$APP" && echo "Deploy everything appimage made" || exit 1
+# make appimage
+VERSION=$(./usr/bin/mpv --version 2>/dev/null | awk 'FNR==1 {print $2}')
+[ -z "$VERSION" ] && echo "ERROR: Could not get version from mpv" && exit 1
+cd .. 
+[ ! -f ./go-appimagetool ] &&  { wget -q "$GOAPPIMAGETOOL" -O ./go-appimagetool || exit 1; }
+[ ! -f ./appimagetool ] && { wget -q "$APPIMAGETOOL" -O ./appimagetool || exit 1; }
+chmod +x ./*tool ./zsyncmake
+./go-appimagetool -s deploy ./mpv.AppDir/usr/share/applications/*.desktop || exit 1
+sed -i 's/export PYTHONHOME/#export PYTHONHOME/g' ./mpv.AppDir/AppRun
+./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 1 \
+  -u "gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|mpv-AppImage|continuous|mpv-*$ARCH.AppImage.zsync" \
+  ./mpv.AppDir mpv-"$VERSION"-"$ARCH".AppImage 
+mv ./*.AppImage* .. && cd .. && rm -rf ./mpv || exit 1
+echo "All done!"
