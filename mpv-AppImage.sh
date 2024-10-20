dca472f75a9a51615fa391255d6fd27a995cf8b2
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-10-20 05:37:44 -0400

    fix bug with yt-dlp

--- mpv-AppImage.sh
+++ mpv-AppImage.sh
@@ -28,9 +28,21 @@ export VERSION=$(./usr/bin/mpv --version 2>/dev/null | awk 'FNR==1 {print $2}')
 [ -z "$VERSION" ] && echo "ERROR: Could not get version from mpv" && exit 1
 cd .. 
 [ ! -f ./go-appimagetool ] && { wget -q "$GOAPPIMAGETOOL" -O ./go-appimagetool || exit 1; }
-chmod +x ./*tool
+chmod +x ./go-appimagetool
 ./go-appimagetool -s deploy ./mpv.AppDir/usr/share/applications/*.desktop || exit 1
+
+# disable this since we are not shipping python
 sed -i 's/export PYTHONHOME/#export PYTHONHOME/g' ./mpv.AppDir/AppRun
+
+# Fix some issue with yt-dlp not working
+# Likely go-appimage breaking something
+for lib in libc.so.6 libdl.so.2 librt.so.1 libpthread.so.0; do
+	rm -f ./mpv.AppDir/usr/lib/x86_64-linux-gnu/"$lib"
+	find / -type f -name "$lib" -exec cp {} ./mpv.AppDir/usr/lib/x86_64-linux-gnu ';' -quit 2>/dev/null
+	patchelf --set-rpath '$ORIGIN' ./mpv.AppDir/usr/lib/x86_64-linux-gnu/"$lib"
+done
+cp /lib64/ld-linux-x86-64.so.2 ./mpv.AppDir/lib64/ld-linux-x86-64.so.2
+
 ./go-appimagetool -s ./mpv.AppDir || exit 1
 mv ./*.AppImage* .. && cd .. && rm -rf ./mpv || exit 1
 echo "All done!"
