f956eb956cd6fd0438bb0dd5ccb97d0e93d27cde
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-10-20 06:14:08 -0400

    use different appimagetool and generate .zsync file

--- mpv-AppImage.sh
+++ mpv-AppImage.sh
@@ -7,11 +7,13 @@
 # rubberband libcaca mesa-egl libxpresent-dev lua5.3-dev libxcb-dev desktop-file-utils
 
 set -u
-export ARCH=x86_64
+export ARCH="$(uname -m)"
 export APPIMAGE_EXTRACT_AND_RUN=1
 REPO="https://github.com/mpv-player/mpv-build.git"
 GOAPPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - \
 	| sed 's/[()",{} ]/\n/g' | grep -oi 'https.*continuous.*tool.*x86_64.*mage$' | head -1)
+APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
+rm -rf ./mpv 2>/dev/null
 mkdir -p ./mpv/mpv.AppDir && cd ./mpv/mpv.AppDir || exit 1
 
 # make vulkan headers
@@ -35,7 +37,8 @@ export VERSION=$(./usr/bin/mpv --version 2>/dev/null | awk 'FNR==1 {print $2}')
 [ -z "$VERSION" ] && echo "ERROR: Could not get version from mpv" && exit 1
 cd .. 
 [ ! -f ./go-appimagetool ] && { wget -q "$GOAPPIMAGETOOL" -O ./go-appimagetool || exit 1; }
-chmod +x ./go-appimagetool
+[ ! -f ./appimagetool ]    && { wget -q "$APPIMAGETOOL" -O ./appimagetool || exit 1; }
+chmod +x ./*tool
 ./go-appimagetool -s deploy ./mpv.AppDir/usr/share/applications/*.desktop || exit 1
 
 # disable this since we are not shipping python
@@ -50,6 +53,10 @@ for lib in libc.so.6 libdl.so.2 librt.so.1 libpthread.so.0; do
 done
 cp /lib64/ld-linux-x86-64.so.2 ./mpv.AppDir/lib64/ld-linux-x86-64.so.2
 
-./go-appimagetool -s ./mpv.AppDir || exit 1
-mv ./*.AppImage* .. && cd .. && rm -rf ./mpv || exit 1
+# maybe not needed but I had appimagetool bug out before if the AppDir isnt in the top leve of home
+mv ./mpv.AppDir ../ && cd ../ || exit 1
+
+./appimagetool --comp zstd \
+	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
+	-n -u "$UPINFO" ./puddletag.AppDir puddletag-"$VERSION"-"$ARCH".AppImage
 echo "All done!"
