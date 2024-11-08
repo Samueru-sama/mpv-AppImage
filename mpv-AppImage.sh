e0aa1a84f03500a6e6772665c307c6de9785b9e4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-11-08 13:22:34 -0400

    migrate to `sharun`

--- mpv-AppImage.sh
+++ mpv-AppImage.sh
@@ -1,59 +1,84 @@
 #!/bin/sh
 
-# dependencies: 
+# dependencies:
 # meson cmake automake ninja ninja-build vulkan-headers freetype-dev libass-dev libtool
-# fribidi-dev harfbuzz-dev yasm libx11 libx11-dev libxinerama-dev libxrandr-dev 
+# fribidi-dev harfbuzz-dev yasm libx11 libx11-dev libxinerama-dev libxrandr-dev
 # libxscrnsaver libxscrnsaver-dev xscreensaver-gl-extras jack libpulse pulseaudio-dev
 # rubberband libcaca mesa-egl libxpresent-dev lua5.3-dev libxcb-dev desktop-file-utils
 
-set -u
+set -eu
 export ARCH="$(uname -m)"
 export APPIMAGE_EXTRACT_AND_RUN=1
 REPO="https://github.com/mpv-player/mpv-build.git"
-GOAPPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - \
-	| sed 's/[()",{} ]/\n/g' | grep -oi 'https.*continuous.*tool.*x86_64.*mage$' | head -1)
 APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
 UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|mpv-AppImage|latest|*$ARCH.AppImage.zsync"
-rm -rf ./mpv 2>/dev/null
-mkdir -p ./mpv/mpv.AppDir && cp ./yt-dlp_hook.sh ./mpv/mpv.AppDir && cd ./mpv/mpv.AppDir || exit 1
+LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+mkdir -p ./mpv/AppDir
+cd ./mpv/AppDir
 
 # Build mpv
-if [ ! -d ./usr ]; then
-	CURRENTDIR="$(readlink -f "$(dirname "$0")")"
-	git clone "$REPO" && cd ./mpv-build || exit 1
-	sed -i "s#meson setup build#meson setup build -Dprefix=$CURRENTDIR/usr#g" ./scripts/mpv-config
-	./rebuild -j$(nproc) && ./install && cd .. && rm -rf ./mpv-build || exit 1
-	cp ./usr/share/icons/hicolor/128x128/apps/mpv.png ./ && ln -s ./mpv.png ./.DirIcon
-fi
-
-export VERSION=$(./usr/bin/mpv --version 2>/dev/null | awk 'FNR==1 {print $2}')
-[ -z "$VERSION" ] && echo "ERROR: Could not get version from mpv" && exit 1
-
-# bundle appdir
+CURRENTDIR="$(readlink -f "$(dirname "$0")")"
+git clone "$REPO" ./mpv-build
+cd ./mpv-build
+sed -i "s#meson setup build#meson setup build -Dprefix=$CURRENTDIR/shared#g" \
+	./scripts/mpv-config
+./rebuild -j$(nproc)
+./install
 cd ..
-[ ! -f ./go-appimagetool ] && { wget -q "$GOAPPIMAGETOOL" -O ./go-appimagetool || exit 1; }
-chmod +x ./go-appimagetool
-./go-appimagetool -s deploy ./mpv.AppDir/usr/share/applications/*.desktop || exit 1
+rm -rf ./mpv-build
+ln -s ./shared ./usr
 
-# disable this since we are not shipping python
-sed -i 's/export PYTHONHOME/#export PYTHONHOME/g' ./mpv.AppDir/AppRun
+# bundle libs
+wget "$LIB4BN" -O ./lib4bin
+chmod +x ./lib4bin
+./lib4bin -p -w -v -s ./shared/bin/mpv
+VERSION=$(./bin/mpv --version 2>/dev/null | awk 'FNR==1 {print $2; exit}')
+if [ -z "$VERSION" ]; then
+	echo "ERROR: Could not get version from mpv"
+	exit 1
+fi
+export VERSION
 
-# make AppRun source the yt-dlp hook
-sed -i '7i\. "$HERE"/yt-dlp_hook.sh' ./mpv.AppDir/AppRun
+# prepare AppDir
+cp ./usr/share/applications/*.desktop ./
+cp ./usr/share/icons/hicolor/128x128/apps/mpv.png ./
+ln -s ./mpv.png ./.DirIcon
+cat >> ./AppRun << 'EOF'
+#!/bin/sh
+CURRENTDIR="$(dirname "$(readlink -f "$0")")"
+export XDG_DATA_DIRS="$CURRENTDIR/usr/share:$XDG_DATA_DIRS"
+export PATH="$CURRENTDIR/bin:$PATH"
+CACHEDIR="${XDG_CACHE_HOME:-$HOME/.cache}"
+export PATH="$PATH:$CACHEDIR/mpv-appimage_yt-dlp"
+export XDG_DATA_DIRS="$CURRENTDIR/usr/share:$XDG_DATA_DIRS"
 
-# launch mpv GUI when double clicking on the appimage
-sed -i '7i\[ -z "$1" ] && set -- "--player-operation-mode=pseudo-gui"' ./mpv.AppDir/AppRun
-
-# go appimage is not stripping the main binary
-strip --strip-unneeded ./mpv.AppDir/usr/bin/mpv || exit 1
-
-# maybe not needed but I had appimagetool bug out before if the AppDir isnt in the top level of home
-mv ./mpv.AppDir ../ && cd ../ || exit 1
+# Download yt-dlp if needed
+if echo "$@" | grep -q "http" && ! command -v yt-dlp >/dev/null 2>&1; then
+	echo "Video link detected but yt-dlp is not installed, installing..."
+	mkdir -p $CACHEDIR"/mpv-appimage_yt-dlp
+	YT="$(curl -Ls https://api.github.com/repos/yt-dlp/yt-dlp/releases \
+		| sed 's/[()",{} ]/\n/g' | grep -oi 'https.*yt.*linux$' | head -1)
+	if command -v wget >/dev/null 2>&1; then
+		wget -q "$YT" -O "$CACHEDIR"/mpv-appimage_yt-dlp/yt-dlp
+	elif command -v curl >/dev/null 2>&1; then
+		curl -Ls "$YT" -o "$CACHEDIR"/mpv-appimage_yt-dlp/yt-dlp
+	else
+		echo "ERROR: You need wget or curl in order to download yt-dlp"
+	fi
+	chmod +x "$CACHEDIR"/mpv-appimage_yt-dlp/yt-dlp
+fi
+[ -z "$1" ] && set -- "--player-operation-mode=pseudo-gui"
+"$CURRENTDIR"/bin/mpv "$@"
+EOF
+chmod +x ./AppRun
 
 # make appimage
-[ ! -f ./appimagetool ] && { wget -q "$APPIMAGETOOL" -O ./appimagetool || exit 1; }
+cd ..
+wget -q "$APPIMAGETOOL" -O ./appimagetool
 chmod +x ./appimagetool
 ./appimagetool --comp zstd \
 	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
-	-n -u "$UPINFO" ./mpv.AppDir mpv-"$VERSION"-"$ARCH".AppImage
+	-n -u "$UPINFO" "$PWD"/AppDir "$PWD"/mpv-"$VERSION"-"$ARCH".AppImage
+mv ./*.AppImage* ../
+cd ..
 echo "All done!"
